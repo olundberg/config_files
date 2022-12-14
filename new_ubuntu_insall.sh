7d5038ac34653c63679fa3c729fc3e86f4e70205
Oscar Lundberg <lundberg.oscar@gmail.com>
2022-12-14 15:06:26 +0100

    Create new_ubuntu_insall.sh

---
+++ new_ubuntu_insall.sh
@@ -0,0 +1,7 @@
+# Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+
+# Sublime text
+sudo apt install sublime-text
