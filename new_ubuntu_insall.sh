c92e2ab7b6f2fa5a0787419818c6b8407fe8f2eb
Oscar Lundberg <lundberg.oscar@gmail.com>
2022-12-14 15:08:21 +0100

    Update new_ubuntu_insall.sh

--- new_ubuntu_insall.sh
+++ new_ubuntu_insall.sh
@@ -5,3 +5,12 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 
 # Sublime text
 sudo apt install sublime-text
+
+# Spotify
+snap install spotify
+
+# Signal
+
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
