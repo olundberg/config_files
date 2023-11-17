d92be7529c559290ca688e952f5955d7e5cd921b
Oscar Lundberg <lundberg.oscar@gmail.com>
2023-11-17 10:43:24 +0100

    New update

--- ubuntu_scripts/toggle_primary_display.sh
+++ ubuntu_scripts/toggle_primary_display.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-# Usuallt M+o hotkey "sh /path/.."
 xrandr --output $(xrandr | grep -w connected | grep -v -e primary | cut -d ' ' -f1) --primary 
+# Usuallt M+o hotkey "sh /path/.."
 #DISPLAY=:0 /usr/bin/notify-send "Changed primary display"
 zenity --notification --text "Changed primary display"
