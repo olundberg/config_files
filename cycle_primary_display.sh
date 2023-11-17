82c9cf911b4e7ca04088b07a396c582d49b41f4b
Oscar Lundberg <lundberg.oscar@gmail.com>
2023-11-17 10:07:34 +0100

    New update

--- cycle_primary_display.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-# Cycle through the primary displays
-# Add to custom keyboard shortcuts and specify "sh path_to_script/script.sh
-xrandr --output $(xrandr | grep -w connected | grep -v -e primary | cut -d ' ' -f1) --primary
