c65df556e6f5d3b3d601ca87e6a8da910d57dff8
Oscar Lundberg <lundberg.oscar@gmail.com>
2023-11-17 10:15:19 +0100

    New update

--- ubuntu_scripts/toggle_do_not_disturb.sh
+++ ubuntu_scripts/toggle_do_not_disturb.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 # Usually M+i
-gsettings set org.gnome.desktop.notifications show-banners false
-zenity --notification --text "DND 1"
+if $(gsettings get org.gnome.desktop.notifications show-banners)
+then
+    gsettings set org.gnome.desktop.notifications show-banners false
+    zenity --notification --text "DND 0"
+else
+    gsettings set org.gnome.desktop.notifications show-banners true
+    zenity --notification --text "DND 1"
+fi
