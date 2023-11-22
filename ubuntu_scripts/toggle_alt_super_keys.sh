140ba144d065211ac53c7747299f9b4e747b43b2
Oscar Lundberg <lundberg.oscar@gmail.com>
2023-11-22 18:43:49 +0100

    New update

---
+++ ubuntu_scripts/toggle_alt_super_keys.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Usually Super+Shift+H
+
+current_output=$(setxkbmap -query | grep option)
+
+if [ "$current_output" = "" ];
+then
+    setxkbmap -option altwin:swap_alt_win
+    zenity --notification --text "Reverse layout"
+else
+    setxkbmap -option
+    zenity --notification --text "Normal layout"
+fi
