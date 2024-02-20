3b9d2b0e635eaef4713db2d46cc0e9845c0848da
OL <oscar.lundberg@gnothis.com>
2024-02-20 15:07:37 +0100

    New update

---
+++ ubuntu_scripts/set_keybindings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Alt>q']"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Alt>w']"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Alt>e']"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Alt>r']"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Alt>t']"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Alt>y']"
+#gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Alt>7']"
+#gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Alt>8']"
+#gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Alt>9']"
