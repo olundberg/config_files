4bf8abc96da8c3943603061b36a895de083a65f3
OL <oscar.lundberg@gnothis.com>
2024-07-04 08:30:16 +0200

    Added Wayland to keyboard config

--- ubuntu_scripts/normal_alt_super_keys.sh
+++ ubuntu_scripts/normal_alt_super_keys.sh
@@ -1,6 +1,13 @@
 #!/bin/bash 
+# Set the normal keys back again
+# To undo reverse_als_super_keys.sh
 # Usually Ctrl+Shift+J
 
+# Only works with X
 setxkbmap -option # altwin:altwin
+
+# Use this if on Gnome (Wayland)
+dconf write /org/gnome/desktop/input-sources/xkb-options "['altwin:swap_lalt_lwin']"
+
 #xmodmap -e "keycode 35 = backslash"
-zenity --notification --text "Normal alt+win, bind pipe to backslash"
+zenity --notification --text "Normal alt+win"
--- ubuntu_scripts/reverse_alt_super_keys.sh
+++ ubuntu_scripts/reverse_alt_super_keys.sh
@@ -1,6 +1,13 @@
 #!/bin/bash 
+# Reverse the ALT and SUPER KEY
+# Reverse this with normal_alt_super_leys.sh
 # Usually Ctrl+Shift+H
 
-setxkbmap -option altwin:swap_lalt_lwin
+# Only works on X
+# setxkbmap -option altwin:swap_lalt_lwin
+
+# For Wayland use this instead
+dconf write /org/gnome/desktop/input-sources/xkb-options "['altwin:swap_lalt_lwin']"
+
 #xmodmap -e "keycode 35 = backslash"
-zenity --notification --text "Reverse alt+win, bind pipe to backslash"
+zenity --notification --text "Reverse alt+win"
