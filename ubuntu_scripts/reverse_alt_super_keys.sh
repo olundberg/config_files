e3a3e1b2f18769258c501f702bff5f2ee2579276
OL <oscar.lundberg@gnothis.com>
2026-05-12 10:50:16 +0200

    New update

--- ubuntu_scripts/reverse_alt_super_keys.sh
+++ ubuntu_scripts/reverse_alt_super_keys.sh
@@ -7,13 +7,13 @@
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
     # For Wayland use this instead
     dconf write /org/gnome/desktop/input-sources/xkb-options "['altwin:swap_lalt_lwin']"
+    zenity --notification --text "Reverse alt+win (Wayland)"
 elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
     # Only works on X
     setxkbmap -option altwin:swap_lalt_lwin
-    zenity --notification --text "Reverse alt+win"
+    zenity --notification --text "Reverse alt+win (X11)"
 else
     echo "Could not detect session type (Environment: $XDG_SESSION_TYPE)"
 fi
 
 #xmodmap -e "keycode 35 = backslash"
-zenity --notification --text "Reverse alt+win"
