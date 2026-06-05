db7f765936bdb6300925a39e4071ae0d60514b7e
OL <oscar.lundberg@gnothis.com>
2026-06-05 20:24:23 +0200

    New update

--- ubuntu_scripts/normal_alt_super_keys.sh
+++ ubuntu_scripts/normal_alt_super_keys.sh
@@ -5,7 +5,15 @@
 
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
     # For Wayland use this instead
+    # 1. Write the keyboard option you want
     dconf write /org/gnome/desktop/input-sources/xkb-options "['altwin:altwin']"
+    # 2. Cache your current active language layout list
+    VAL=$(gsettings get org.gnome.desktop.input-sources sources)
+    # 3. Change it to an empty list (forces a momentary drop/reset of input handling)
+    gsettings set org.gnome.desktop.input-sources sources "[]"
+    # 4. Put your original layout back instantly
+    gsettings set org.gnome.desktop.input-sources sources "$VAL"
+    # Print notificatoin
     zenity --notification --text "Normal alt+win (Wayland)"
 elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
     # Only works on X
