614db2af67483c562698d0e58e556d417e7cc165
OL <oscar.lundberg@gnothis.com>
2026-06-11 20:09:01 +0200

    New update

---
+++ ubuntu_scripts/turn_off_nvidia.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+# Set the normal keys back again
+# To undo reverse_als_super_keys.sh
+# Usually Ctrl+Shift+J
+
+sudo prime-select intel
+zenity --notification --text "Turned off Nvidia, reboot computer"
